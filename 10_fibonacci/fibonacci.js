277b23cb248f451f8d967600e9bb4e132f713ba7
Ben <benperry1915@gmail.com>
2023-09-13 13:40:52 +0100

    Finish the JS fundamental part 4 exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    index = parseInt(index);
+    if (index < 0) return "OOPS";
+        
+    if (index === 0) return 0;
+    if (index === 1) return 1;
 
+    let arr = [0,1];
+    for (i = 2; i <= index; i++) {
+        temp = arr[i-1] + arr[i-2];
+        arr.push(temp);
+    }
+    return arr.pop();
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((result, person) => {
+        let age1 = getAge(person.yearOfDeath, person.yearOfBirth);
+        let age2 = getAge(result.yearOfDeath, result.yearOfBirth);
 
-};
+        return age1 > age2 ? person : result;
+    });
 
-// Do not edit below this line
+}
+
+const getAge = function (death, birth) {
+    if(!death) {
+        death = new Date().getFullYear();
+    }
+    return death-birth;
+}
+
+    // Do not edit below this line
 module.exports = findTheOldest;
